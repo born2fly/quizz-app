9ae57dc06b181ecd2bda55ae836eda9e1bf1d72a
Rich Leonard <rich@homeimprovement-li.com>
2017-02-01 10:53:56 -0500

    work in progress

--- app.js
+++ app.js
@@ -1,88 +1,74 @@
+var currentQuest = 0;
+var score = 0;
 
-	var currentQuest = 0;
-	var score = 0;
+var quizQuestion = [{
+		question: "What does the typical framing hammer weigh?",
+		choices: ["12oz.", "16oz.", "22oz."],
+		answer: 2
+	}, {
 
-var quizQuestion = [
-	 {
-			 question:"What does the typical framing hammer weigh?",
-		     choices: ["12oz.","16oz.","22oz."],
-		     answer: 2
-	 },{
-
-			 question:"How is concrete measured?",
-			 choices: ["lbs","cubic foot","square foot"],
-		     answer: 1
-	 },{
-			 question:"How many square feet in a sheet of plywood?",
-			 choices: ["16","24","32"],
-		     answer: 2
-	 },{
-			 question:"How tall is an 8' wall?",
-			 choices: ["8 feet","8 feet 2 inches","8 feet 6 inches"],
-		     answer: 0
-	 },{
-			 question:"Standard wall framing places studs on center what distance?",
-			 choices: ["12 inches", "16 inches","18 inches"],
-		     answer: 1
-     }
-     
-	 ];
-		
-function showQuestion(num){
-	console.log(num);
-	
-	if (num < quizQuestion.length){ 
-		$('.displayquestion').text(quizQuestion[num].question);
-	showChoices(num);
+		question: "How is concrete measured?",
+		choices: ["lbs", "cubic foot", "square foot"],
+		answer: 1
+	}, {
+		question: "How many square feet in a sheet of plywood?",
+		choices: ["16", "24", "32"],
+		answer: 2
+	}, {
+		question: "How tall is an 8' wall?",
+		choices: ["8 feet", "8 feet 2 inches", "8 feet 6 inches"],
+		answer: 0
+	}, {
+		question: "Standard wall framing places studs on center what distance?",
+		choices: ["12 inches", "16 inches", "18 inches"],
+		answer: 1
 	}
-	else {
+
+];
+
+function showQuestion(num) {
+	console.log(num);
+
+	if (num < quizQuestion.length) {
+		$('.displayquestion').text(quizQuestion[num].question);
+		showChoices(num);
+	} else {
 		$('.displayquestion').text("That's all folks !");
 		$('.displaychoices').hide();
-	     } 
+	}
 	// console.log(showQuestion());
 }
 
-function showChoices(num){
-    var getChoices = quizQuestion[num].choices;
-    var choiceList = '';
+function showChoices(num) {
+	var getChoices = quizQuestion[num].choices;
+	var choiceList = '';
 	for (var i = 0; i < getChoices.length; i++) {
-		choiceList += '<li><input type = "radio" name = "choices" value = " '+i+' " /><label>' + getChoices[i] + "</label></li>";
+		choiceList += '<li><input type = "radio" name = "choices" value = " ' + i + ' " /><label>' + getChoices[i] + "</label></li>";
+	}
+	$('.displaychoices').html(choiceList);
 }
 
-	$('.displaychoices').html(choiceList);
-				
-	}
-
-
-function checkAnswer(guess){
+function checkAnswer(guess) {
 	var question = quizQuestion[currentQuest];
-	if(guess == question.answer){
-		score++;}
-	
-
-	$('.current-score').text("Your score is --> " +score);
-
+	if (guess == question.answer) {
+		score++;
 	}
-  
-		      
-$(document).ready(function() {
+	$('.current-score').text("Your score is --> " + score);
+}
 
-$(".start-quiz").click(function(event) {
-	showQuestion(currentQuest);
-	 console.log("you clicked the start button");
-	
-});
+$(document).ready(function () {
 
-$(".submit-button").click(function(event) {
-	var userAnswer = $('input[type="radio"]:checked').attr('value');
- 	 console.log(userAnswer);
- checkAnswer(userAnswer);
- currentQuest++;
- showQuestion(currentQuest); 
+	$(".start-quiz").click(function (event) {
+		showQuestion(currentQuest);
+		console.log("you clicked the start button");
+	});
 
-         
-});
+	$(".submit-button").click(function (event) {
+		var userAnswer = $('input[type="radio"]:checked').attr('value');
+		console.log(userAnswer);
+		checkAnswer(userAnswer);
+		currentQuest++;
+		showQuestion(currentQuest);
+	});
 
 });
-
-
